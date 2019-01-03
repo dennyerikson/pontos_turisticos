185e0cece2bb2309fa5008b8e474ba87258de05e
dennyerikson <dennyeriks00on@gmail.com>
2019-01-03 15:57:15 -0200

    actions personalizadas e implementando imagens na API

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,10 +1,35 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 
-class PontouristicoViewSet(ModelViewSet):
+class PontoTuristicoViewSet(ModelViewSet):
 
     serializer_class = PontoTuristicoSerializer
 
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
+
+    def list(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        return super(PontouristicoViewSet, self).retrieve(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        return super(PontouristicoViewSet, self).update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+        return super(PontouristicoViewSet, self).partial_update(request, *args, **kwargs)
+
+    @action(methods=['post', 'get'], detail=True)
+    def denunciar(self, request, pk=None):
+        pass
+
+    @action(methods=['get'], detail=False)
+    def teste(self, request):
+        pass
--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from core.api.viewsets import PontouristicoViewSet
+from django.conf import settings
+from django.conf.urls.static import static
+from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracoesViewSet
 from enderecos.api.viewsets import EnderecosViewSet
 from comentarios.api.viewsets import ComentariosViewSet
@@ -24,7 +26,7 @@ from avaliacoes.api.viewsets import AvaliacaoViewSet
 
 
 router = routers.DefaultRouter()
-router.register(r'pontoturistico', PontouristicoViewSet, base_name='PontoTuristico')
+router.register(r'pontosturisticos', PontoTuristicoViewSet, base_name='PontoTuristico')
 router.register(r'atracoes', AtracoesViewSet)
 router.register(r'enderecos', EnderecosViewSet)
 router.register(r'comentarios', ComentariosViewSet)
@@ -33,4 +35,4 @@ router.register(r'avaliacoes', AvaliacaoViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
