fdac58d8ab0497dcfac1541b56c4d61a7f707db8
dennyerikson <dennyeriks00on@gmail.com>
2019-01-08 19:41:54 -0200

    Incrementando objeto com NestedRelationShips. Incluir informaçoes adicionais com SerializerMethodField e Property

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework.serializers import ModelSerializer
 from core.models import PontoTuristico
+from atracoes.api.serializers import AtracaoSerializer
+from enderecos.api.serializers import EnderecoSerializer
+
 
 class PontoTuristicoSerializer(ModelSerializer):
+
+    atracoes = AtracaoSerializer(many=True)
+    endereco = EnderecoSerializer()
+
     class Meta:
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao', 'aprovado', 'fotos')
+        fields = ('id', 'nome', 'descricao', 'aprovado', 'fotos',
+                  'atracoes', 'comentarios', 'avaliacoes', 'endereco'
+                  )
